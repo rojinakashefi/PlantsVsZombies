380478be92b7d1b6d93b3091cc26b35372c6d1fe
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:40:40 +0330

    constructor of PeaShooter class has completed

--- src/Objects/Plants/PeaShooter.java
+++ src/Objects/Plants/PeaShooter.java
@@ -7,5 +7,13 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class PeaShooter extends Plant {
+    public static ArrayList<PeaShooter> peaShooters = new ArrayList<>();
 
+    public PeaShooter(Container c) {
+        super(c, 100, 1);
+        ready = new ImageIcon("gfx/pea.pvz");
+        setIcon(ready);
+        setLocation(100, 1000);
+        PeaShooter.peaShooters.add(this);
+    }
 }
