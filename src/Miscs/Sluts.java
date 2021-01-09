00d62e57cb1a5d686a1ccde6c15b9136f49a6690
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:43:28 +0330

    constructor of Wall_nut class has completed

--- src/Miscs/Sluts.java
+++ src/Miscs/Sluts.java
@@ -1,11 +1,5 @@
 package Miscs;
 
-import Objects.Plants.PeaBullet;
-import Objects.Plants.Plant;
-import Objects.Zombies.Zombie;
-
-import java.awt.*;
-
 public class Sluts {
 
 }
--- src/Objects/Plants/Wall_nut.java
+++ src/Objects/Plants/Wall_nut.java
@@ -5,5 +5,14 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Wall_nut extends Plant {
+    public static ArrayList<Wall_nut> wall_nuts = new ArrayList<>();
+
+    public Wall_nut(Container c) {
+        super(c, 100, 0);
+        ready = new ImageIcon("gfx/nut_1.pvz");
+        setIcon(ready);
+        setLocation(100, 1000);
+        Wall_nut.wall_nuts.add(this);
+    }
 }
 
