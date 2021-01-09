14c5f489ca5632b767d7c25ad4663878d002cdb9
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:41:12 +0330

    constructor of Repeater class has completed

--- src/Objects/Plants/Repeater.java
+++ src/Objects/Plants/Repeater.java
@@ -5,5 +5,13 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Repeater extends Plant {
+    public static ArrayList<Repeater> repeaters = new ArrayList<>();
 
+    public Repeater (Container c) {
+        super(c, 100, 2);
+        ready = new ImageIcon("gfx/repeater.pvz");
+        setIcon(ready);
+        setLocation(100, 1000);
+        Repeater.repeaters.add(this);
+    }
 }
