d0174e56493b645dc0701c40cc304010e77b5e3c
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:39:00 +0330

    constructor of Cherry class has completed

--- src/Objects/Plants/Cherry.java
+++ src/Objects/Plants/Cherry.java
@@ -5,4 +5,13 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Cherry extends Plant {
+    public static ArrayList<Cherry> cherries = new ArrayList<>();
+
+    public Cherry(Container c) {
+        super(c, 100, 3);
+        ready = new ImageIcon("gfx/cherry.pvz");
+        setIcon(ready);
+        setLocation(100, 1000);
+        Cherry.cherries.add(this);
+    }
 }
