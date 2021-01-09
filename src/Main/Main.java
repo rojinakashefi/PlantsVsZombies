571001ebf3977fad4fc97be35790fa4c90141d62
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:36:07 +0330

    Main package & class added

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Menus.MainMenu;
+
+public class Main {
+    public static final boolean TESTING = true;
+    public static void main(String[] args) {
+        new MainMenu();
+    }
+}
