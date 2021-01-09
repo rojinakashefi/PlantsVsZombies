f8e902cc44791a6a0b2f52e7fea24f0c46276e23
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:39:57 +0330

    constructor of PeaBullet class has completed

--- src/Objects/Plants/PeaBullet.java
+++ src/Objects/Plants/PeaBullet.java
@@ -1,6 +1,7 @@
 package Objects.Plants;
 
 import Menus.Game;
+import Miscs.Sluts;
 import Objects.Zombies.Zombie;
 
 import javax.swing.*;
@@ -8,6 +9,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static Main.Main.TESTING;
+
 
 public class PeaBullet extends JLabel {
+    boolean isMoving = false;
+    public PeaBullet (Container c, Plant origin, boolean isFrozen) {
+        //c.add(this);
+
+    }
+
 }
