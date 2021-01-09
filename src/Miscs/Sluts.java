f5c5fe3c431852b942e544bfe9508823a913306b
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:47:25 +0330

    Essential methods added to Slut class

--- src/Miscs/Sluts.java
+++ src/Miscs/Sluts.java
@@ -1,5 +1,41 @@
 package Miscs;
 
-public class Sluts {
+import Objects.Plants.PeaBullet;
+import Objects.Plants.Plant;
+import Objects.Zombies.Zombie;
 
+import java.awt.*;
+
+public class Sluts {
+    static final int FIRST_X = 260, FIRST_Y = 80, X = 10, Y = 5;
+    static int[][][] sluts = new int[X][Y][2];
+
+    public static void setSluts() {
+
+    }
+
+    public static int[] getPlantLocation(int X, int Y) {
+
+    }
+
+    public static int[] getZombieLocation(int Y) {
+
+    }
+
+    public static int[] getSlut(int posX, int posY) {
+
+    }
+    public static int[] getSlut(Point p) {
+
+    }
+    public static int[] getSlut(Plant p) {
+
+    }
+
+    public static int[] getSlut(PeaBullet p) {
+
+    }
+    //public static int[] getSlut(Mower p) {
+    //   return getSlut(p.x, p.y);
+    //}
 }
