94ccb95bbabcfaf9259a9d6ba78754e109e23f99
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-09 18:42:13 +0330

    constructor of SunFlower class has completed

--- src/Objects/Plants/SunFlower.java
+++ src/Objects/Plants/SunFlower.java
@@ -5,6 +5,13 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class SunFlower extends Plant {
+    public static ArrayList<SunFlower> sunFlowers = new ArrayList<>();
 
-
+    public SunFlower(Container c) {
+        super(c, 100, 20);
+        ready = new ImageIcon("gfx/sunflower.pvz");
+        setIcon(ready);
+        setLocation(100, 1000);
+        SunFlower.sunFlowers.add(this);
+    }
 }
