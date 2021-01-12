293b10609e2b04c6805653f01ccb125748238f67
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-12 19:06:14 +0330

    Adding bucketHead zombie constructor

--- src/Objects/Zombies/BucketHead.java
+++ src/Objects/Zombies/BucketHead.java
@@ -1,4 +1,17 @@
 package Objects.Zombies;
 
-public class BucketHead {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BucketHead extends Zombie {
+    public static ArrayList<Zombie> BucketHead = new ArrayList<>();
+
+    public BucketHead(Container c) {
+        super(c, 100, 1, 30);
+        walk = new ImageIcon("gfx/bucket.pvz");
+        die = new ImageIcon("gfx/normDead.pvz");
+        setIcon(walk);
+        Normal.zombies.add(this);
+    }
 }
