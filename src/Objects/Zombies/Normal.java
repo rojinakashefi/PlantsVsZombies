52ea44529ff8455e7dab3f5b17fc209481dc2f79
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-12 18:56:51 +0330

    Adding normal zombie constructor

--- src/Objects/Zombies/Normal.java
+++ src/Objects/Zombies/Normal.java
@@ -4,5 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Normal {
+public class Normal extends Zombie {
+    public static ArrayList<Zombie> zombies = new ArrayList<>();
+
+    public Normal(Container c) {
+        super(c, 100, 1, 30);
+        walk = new ImageIcon("gfx/normal.pvz");
+        die = new ImageIcon("gfx/normDead.pvz");
+        setIcon(walk);
+        Normal.zombies.add(this);
+    }
 }
