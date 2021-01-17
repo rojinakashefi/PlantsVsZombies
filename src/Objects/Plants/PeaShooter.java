cb07fa15f4a04a018ab3a86b94ab94ea04c82038
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-17 14:08:11 +0330

    PeaShooter edited(the number of health)

--- src/Objects/Plants/PeaShooter.java
+++ src/Objects/Plants/PeaShooter.java
@@ -4,7 +4,6 @@ import javax.swing.ImageIcon;
 import java.awt.Container;
 
 public class PeaShooter extends Plant {
-
     public PeaShooter(Container c, int[] slut) {
         super(c, 100, 1, slut[0], slut[1], 100);
         ready = new ImageIcon("gfx/pea.pvz");
