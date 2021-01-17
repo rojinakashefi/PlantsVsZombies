90ab6b2c6f0fb9a1dac4bb55827a1be95651a7d2
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-17 13:47:01 +0330

    Repeater class edited

--- src/Objects/Plants/Repeater.java
+++ src/Objects/Plants/Repeater.java
@@ -1,17 +1,13 @@
 package Objects.Plants;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import java.awt.Container;
 
 public class Repeater extends Plant {
-    public static ArrayList<Repeater> repeaters = new ArrayList<>();
 
-    public Repeater (Container c) {
-        super(c, 100, 2);
+    public Repeater (Container c, int[] slut) {
+        super(c, 100, 2, slut[0], slut[1], 200);
         ready = new ImageIcon("gfx/repeater.pvz");
         setIcon(ready);
-        setLocation(100, 1000);
-        Repeater.repeaters.add(this);
     }
 }
