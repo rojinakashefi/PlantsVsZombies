3cb7a8f7593002193567bd8b0c07222ebd760c54
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-17 13:48:24 +0330

    SnowPea class edited

--- src/Objects/Plants/SnowPea.java
+++ src/Objects/Plants/SnowPea.java
@@ -1,17 +1,16 @@
 package Objects.Plants;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import java.awt.Container;
+
 
 public class SnowPea extends Plant {
-    public static ArrayList<SnowPea> snowPeas = new ArrayList<>();
 
-    public SnowPea(Container c) {
-        super(c, 100, 1);
+    public SnowPea(Container c, int[] slut) {
+        super(c, 100, 1, slut[0], slut[1], 175);
         ready = new ImageIcon("gfx/snowPea.pvz");
         setIcon(ready);
         setLocation(100, 1000);
-        SnowPea.snowPeas.add(this);
+        Plant.plants.add(this);
     }
 }
