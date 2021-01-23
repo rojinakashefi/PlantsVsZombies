7e1e2a1ad6dbc2de1cc2e5579850743e6f01b5be
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-23 11:45:21 +0330

    Level class completed(Gson edited & add to the library)

--- src/Miscs/Levels.java
+++ src/Miscs/Levels.java
@@ -1,5 +1,6 @@
 package Miscs;
 
+import Menus.Game;
 import com.google.gson.Gson;
 
 import java.io.*;
@@ -7,7 +8,7 @@ import java.io.*;
 import static Main.Main.TESTING;
 
 public class Levels {
-    public int level, type, difficulty;
+    public int level, difficulty;
 
     public Levels() {
         new Levels(1, 1);
@@ -16,39 +17,48 @@ public class Levels {
     public Levels(int level, int difficulty) {
         this.level = level;
         this.difficulty = difficulty;
-        type = 1;
     }
-    package Miscs;
-
-import com.google.gson.Gson;
-
-import java.io.*;
-
-import static Main.Main.TESTING;
-
-    public class Levels {
-        public int level, type, difficulty;
-
-        public Levels() {
-            new Miscs.Levels(1, 1);
+    public void gameSave(Game game) {
+        Writer out;
+        try {
+            out = new FileWriter("lastSave.json", false);
+            Gson jsonWriter = new Gson();
+            jsonWriter.toJson(game, out);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        public Levels(int level, int difficulty) {
-            this.level = level;
-            this.difficulty = difficulty;
-            type = 1;
+        if (TESTING) System.out.println("File Sent!");
+    }
+    public void save() {
+        Writer out;
+        try {
+            out = new FileWriter("save.json", false);
+            Gson jsonWriter = new Gson();
+            jsonWriter.toJson(this, out);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        public void save() {
-            Writer out;
+        if (TESTING) System.out.println("File Sent!");
+    }
+    public static Levels load() {
+        Reader in;
+        Levels temp = null;
+        if (seek()) {
             try {
-                out = new FileWriter("save.json", false);
-                Gson jsonWriter = new Gson();
-                jsonWriter.toJson(this, out);
-                out.flush();
-                out.close();
+                in = new FileReader("save.json");
+                Gson jsonReader = new Gson();
+                temp = jsonReader.fromJson(in, Levels.class);
+                in.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if (TESTING) System.out.println("File Sent!");
-        }
+            if (TESTING) System.out.println("File Loaded!");
+            return temp;
+        } else return null;
+    }
+    static boolean seek() { return new File("save.json").exists(); }
 }
