6a18b807e71468ea84498e61777458b2c4a76710
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-23 21:07:48 +0330

    Changing ConeHead Zombie constructor

--- src/Objects/Zombies/ConeHead.java
+++ src/Objects/Zombies/ConeHead.java
@@ -1,5 +1,7 @@
 package Objects.Zombies;
 
+import Miscs.Sounds;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -7,14 +9,15 @@ import java.util.ArrayList;
 public class ConeHead extends Zombie {
     public static ArrayList<Zombie> ConeHead = new ArrayList<>();
 
-    public ConeHead(Container c) {
-        super(c, 100, 1, 30);
+    public ConeHead(Container c, int row) {
+        super(c, 560, 2, 30, row);
         walk = new ImageIcon("gfx/cone.pvz");
         die = new ImageIcon("gfx/normDead.pvz");
         setIcon(walk);
-        Normal.zombies.add(this);
+        sizeX = 100;
+        sizeY = 100;
+        hitSound = Sounds.PLASTIC;
     }
-
     @Override
     public void lossHealth(int Amount, boolean isFrozen) {
 
