6fad5ef98220ca7855103f1b66b9969b7c845493
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-24 13:12:58 +0330

    SunPoint class completed

--- src/Objects/Plants/SunPoint.java
+++ src/Objects/Plants/SunPoint.java
@@ -1,4 +1,17 @@
 package Objects.Plants;
 
-public class SunPoint {
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Container;
+
+public class SunPoint extends JLabel {
+    public final int points = 25;
+
+    public SunPoint(Container c) {
+        this.setIcon(new ImageIcon("gfx/sun.pvz"));
+        c.add(this);
+    }
+    public void remove() {
+        System.runFinalization();
+    }
 }
