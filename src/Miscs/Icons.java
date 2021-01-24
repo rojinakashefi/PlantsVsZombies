a016d8c1849918ae3fd6dcf7c6ce6c4fefa44aa5
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-24 20:18:46 +0330

    Adding Icons class(importing static pics)

---
+++ src/Miscs/Icons.java
@@ -0,0 +1,25 @@
+package Miscs;
+
+import javax.swing.*;
+
+public class Icons {
+    public static ImageIcon buttonIcon = new ImageIcon("gfx/button.pvz");
+    public static ImageIcon mowerIcon = new ImageIcon("gfx/mower.pvz");
+    public static ImageIcon plantMenuIcon = new ImageIcon("gfx/pm.pvz");
+    public static ImageIcon snowBulletIcon = new ImageIcon("gfx/snowBullet.pvz");
+    public static ImageIcon peaBulletIcon = new ImageIcon("gfx/bullet.pvz");
+    public static ImageIcon scoreBoxIcon = new ImageIcon("gfx/score.pvz");
+    public static ImageIcon pauseButtonIcon = new ImageIcon("gfx/pause.pvz");
+    public static ImageIcon blackScreen = new ImageIcon("gfx/blackScreen.pvz");
+    public static ImageIcon pauseMenu = new ImageIcon("gfx/pauseFrame.pvz");
+    public static ImageIcon sunflowerIcon = new ImageIcon("gfx/sunflower.pvz");
+    public static ImageIcon peaIcon = new ImageIcon("gfx/pea.pvz");
+    public static ImageIcon frozenIcon = new ImageIcon("gfx/snowPea.pvz");
+    public static ImageIcon walnutIcon = new ImageIcon("gfx/nut_1.pvz");
+    public static ImageIcon cherryIcon = new ImageIcon("gfx/cherry.pvz");
+    public static ImageIcon background = new ImageIcon("gfx/back.pvz");
+    public static ImageIcon constantZombieIcon = new ImageIcon("gfx/zombies.pvz");
+    public static ImageIcon closeDialogIcon = new ImageIcon("gfx/closeD.pvz");
+    public static ImageIcon saveNExitIcon = new ImageIcon("gfx/saveNexit.pvz");
+    public static ImageIcon quitGameIcon = new ImageIcon("gfx/quit.pvz");
+}
