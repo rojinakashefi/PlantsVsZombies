87c11b8b2589b59e8207ff7c7b049b4ab7b716cf
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-24 12:58:45 +0330

    essential field & methods added to the cards class

--- src/Miscs/Cards.java
+++ src/Miscs/Cards.java
@@ -5,4 +5,17 @@ import javax.swing.JLabel;
 import java.awt.Container;
 
 public class Cards {
+    public static final int SNOW_PEA = 0, PEA_SHOOTER = 1, SUNFLOWER = 2, WALL_NUT = 3, CHERRY = 4;
+    static final ImageIcon snowPea = new ImageIcon("gfx/cardFreeze.pvz");
+    static final ImageIcon peaShooter = new ImageIcon("gfx/cardPea.pvz");
+    // static final ImageIcon repeater = new ImageIcon("gfx/cardFreeze.pvz");
+    static final ImageIcon sunflower = new ImageIcon("gfx/cardSun.pvz");
+    static final ImageIcon wall = new ImageIcon("gfx/cardWallnut.pvz");
+    static final ImageIcon cherry = new ImageIcon("gfx/cardCherry.pvz");
+
+    public static JLabel getCard(int number, Container c) {
+
+        }
+
+
 }
