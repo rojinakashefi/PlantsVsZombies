5f457d49b291f7b11eebcac6624c8f3d6e9a3b31
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-26 20:38:17 +0330

    Adding PauseMenu constructor(close button,...)

--- src/Menus/PauseMenu.java
+++ src/Menus/PauseMenu.java
@@ -1,5 +1,8 @@
 package Menus;
 
+import Miscs.Icons;
+import Miscs.Sounds;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,4 +14,24 @@ public class PauseMenu extends JFrame {
     JLabel quitButton = new JLabel();
     JLabel saveButton = new JLabel();
     Game mainGame;
+    public PauseMenu(Game game){
+        mainGame = game;
+        quitButton.setIcon(Icons.quitGameIcon);
+        closeButton.setIcon(Icons.closeDialogIcon);
+        saveButton.setIcon(Icons.saveNExitIcon);
+        pane.setLayout(layout);
+        JLabel pauseFrame = new JLabel();
+        pauseFrame.setIcon(Icons.pauseMenu);
+        pane.add(pauseFrame);
+        muteCheckBox.setSelected(!Game.mute);
+
+        muteCheckBox.addChangeListener(e -> {
+            Game.mute = !muteCheckBox.isSelected();
+            if (!muteCheckBox.isSelected()) Sounds.mute();
+            else {
+                Sounds.muted = false;
+                Sounds.backPlay(Sounds.IN_GAME);
+            }
+        });
+    }
 }
