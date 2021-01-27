eb5043720169bad90abb3f5a92fb557839cd80cf
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-27 11:11:55 +0330

    Adding addClickListener methods in SettingMenu class

--- src/Menus/SettingMenu.java
+++ src/Menus/SettingMenu.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class SettingMenu extends JFrame{
+public class SettingMenu extends JFrame {
     SpringLayout layout = new SpringLayout();
     Container pane = this.getContentPane();
     JCheckBox muteCheckBox = new JCheckBox("Sounds and Music");
@@ -17,4 +17,91 @@ public class SettingMenu extends JFrame{
     JLabel nameLBL = new JLabel("UserName :");
     JTextField nameTXT;
     MainMenu menu;
+
+    private void addClickListeners() {
+        modeNButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                menu.mode = 0;
+                modeNButton.setIcon(Icons.modeNOffIcon);
+                modeHButton.setIcon(Icons.modeHIcon);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        modeHButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                menu.mode = 1;
+                modeNButton.setIcon(Icons.modeNIcon);
+                modeHButton.setIcon(Icons.modeHOffIcon);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        closeButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (nameTXT.getText().equals("")) nameTXT.setText(menu.username);
+                menu.username = nameTXT.getText();
+                dispose();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
 }
