05459f5a0c165e6df9c99cf61b514c690bcb7a45
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-29 12:40:20 +0330

    Completing Threepeater class

--- src/Objects/Plants/Threepeater.java
+++ src/Objects/Plants/Threepeater.java
@@ -1,4 +1,13 @@
 package Objects.Plants;
 
-public class Threepeater {
+import javax.swing.*;
+import java.awt.*;
+
+public class Threepeater extends Plant{
+    public Threepeater (Container c, int[] slut) {
+        super(c, 200, 1, slut[0], slut[1], 325);
+        ready = new ImageIcon("gfx/threepeater.pvz");
+        setIcon(ready);
+    }
+
 }
