174ea1d4a44bf503072675f58325e3ab0de9892b
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-29 13:09:45 +0330

    Completing findPlayerIndex (Main class)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,6 +13,11 @@ public class Main {
     public static void main(String[] args) {
     }
     public static int findPlayerIndex(String name) {
-
+        for (int i = 0; i < loadedPlayers.size(); i++) {
+            if (loadedPlayers.get(i).username.equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
