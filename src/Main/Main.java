cca16c0c5aa930f084e35d23572212d9be7604db
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-29 13:09:29 +0330

    Adding findPlayerIndex (Main class)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,18 @@
 package Main;
 
-import Menus.MainMenu;
+import Miscs.Levels;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    public static ArrayList<Levels> loadedPlayers = new ArrayList<>();
     public static final boolean TESTING = true;
+
     public static void main(String[] args) {
-        new MainMenu();
+    }
+    public static int findPlayerIndex(String name) {
+
     }
 }
