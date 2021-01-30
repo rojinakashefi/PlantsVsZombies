fa571b17326b78ce938ef99bb235f0c49a780a37
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-30 09:15:04 +0330

    RankingMenu GUI completed

--- src/Menus/RankingMenu.java
+++ src/Menus/RankingMenu.java
@@ -1,4 +1,44 @@
 package Menus;
 
-public class RankingMenu {
+
+import Main.Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RankingMenu extends JFrame {
+
+    public RankingMenu() {
+        SpringLayout layout = new SpringLayout();
+        this.setLayout(layout);
+        this.setSize(480, 465);
+        this.setLocationRelativeTo(null);
+        Container This = getContentPane();
+        if (!Main.loadedPlayers.isEmpty()) {
+            String[] columns = new String[] {
+                    "Row", "Name", "Score", "Wins", "Losses"
+            };
+
+            Object[][] data = new Object[Main.loadedPlayers.size()][5];
+            for (int i = 0; i < data.length; i++) {
+                data[i][0] = i + 1;
+                data[i][1] = Main.loadedPlayers.get(i).username;
+                data[i][2] = Main.loadedPlayers.get(i).score;
+                data[i][3] = Main.loadedPlayers.get(i).wins;
+                data[i][4] = Main.loadedPlayers.get(i).losses;
+            }
+
+            JTable table = new JTable(data, columns);
+
+            this.add(new JScrollPane(table));
+
+
+        } else {
+            JLabel label = new JLabel("No Data Available");
+            this.add(label);
+            layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, label, 0, SpringLayout.HORIZONTAL_CENTER, This);
+            layout.putConstraint(SpringLayout.VERTICAL_CENTER, label, 0, SpringLayout.VERTICAL_CENTER, This);
+        }
+
+    }
 }
