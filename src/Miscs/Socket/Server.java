52661fdf22ee22b95c3e074405d35b0f4293c511
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-31 11:32:50 +0330

    Completing Server constructor (Server class)

--- src/Miscs/Socket/Server.java
+++ src/Miscs/Socket/Server.java
@@ -22,9 +22,25 @@ public class Server {
 
 
     public Server() {
-
+        Thread server_activation = new Thread(() -> {
+            try {
+                listener = new ServerSocket(port);
+                if (TESTING) System.out.println("[SERVER] Server Initialized.");
+                while (isServerUp) {
+                    Socket client = listener.accept();
+                    if (TESTING) System.out.println("[SERVER] Connected To " + Client.Names.get(pointer) + "!");
+                    ClientHandler clientThread = new ClientHandler(client);
+                    clients.put(Client.Names.get(pointer++), clientThread);
+                    ClientPool.execute(clientThread);
+                    clientThread.server = this;
+                    clientThread.num = i++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        server_activation.start();
     }
-
 
     public void send(String SenderType, String ReceiverType, String[] Data) {
 
