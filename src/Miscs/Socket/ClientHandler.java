a70662bdce00f15b8f0222b4a2a42d1b31b98155
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-31 11:30:01 +0330

    Adding essential fields & methods (ClientHandler class)

--- src/Miscs/Socket/ClientHandler.java
+++ src/Miscs/Socket/ClientHandler.java
@@ -1,4 +1,35 @@
 package Miscs.Socket;
 
-public class ClientHandler {
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static Main.Main.TESTING;
+import static Main.Main.isServerUp;
+
+
+public class ClientHandler implements Runnable {
+    Socket client;
+    Server server;
+    BufferedReader input;
+    PrintWriter out;
+    int num;
+
+    public ClientHandler(Socket client) throws IOException {
+
+    }
+
+    @Override
+    public void run() {
+
+        }
+    }
+
+    private void ServerReceiver() throws IOException {
+
+        }
+    }
 }
