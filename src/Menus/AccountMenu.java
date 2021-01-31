c331ea7f09057b9dbef8077534732aed2418ed0f
rojinakashefi <rojina.kashefi@yahoo.com>
2021-01-31 09:20:24 +0330

    Adding window listener in account menu constructor

--- src/Menus/AccountMenu.java
+++ src/Menus/AccountMenu.java
@@ -6,6 +6,8 @@ import Miscs.Socket.Client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import static Main.Main.findPlayerIndex;
 import static Main.Main.loadedPlayers;
@@ -42,6 +44,44 @@ public class AccountMenu extends JFrame implements Runnable  {
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                account.close();
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+
+        });
 
     }
     private void receive() {
