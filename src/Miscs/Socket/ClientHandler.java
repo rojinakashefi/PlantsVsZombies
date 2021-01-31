2d39fa0b54142948518076ce85257c018ca60b89
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-31 11:31:31 +0330

    Completing ServerReceiver method (ClientHandler class)

--- src/Miscs/Socket/ClientHandler.java
+++ src/Miscs/Socket/ClientHandler.java
@@ -44,7 +44,32 @@ public class ClientHandler implements Runnable {
     }
 
     private void ServerReceiver() throws IOException {
-
+        String SenderType;
+        SenderType = input.readLine();
+        if (Client.Names.size() >= 2) {
+            String ReceiverType = input.readLine();
+            if (ReceiverType != null) {
+                if (!ReceiverType.equals("Close")) {
+                    int Number = Integer.parseInt(input.readLine());
+                    String[] Data = new String[Number];
+                    for (int i = 0; i < Data.length; i++)
+                        Data[i] = input.readLine();
+                    server.send(SenderType, ReceiverType, Data);
+                    ServerReceiver();
+                } else {
+                    if (TESTING) System.out.println("[HANDLER] Closing...");
+                    server.deleteClient(SenderType);
+                    Client.Names.remove(SenderType);
+                }
+            }
+        } else {
+            if (TESTING) System.out.println("[HANDLER] Failed To Send. Trying Again!");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ServerReceiver();
         }
     }
 }
