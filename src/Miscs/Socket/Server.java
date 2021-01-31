0e82492d0144e3dcdd1c814cd09d57c0791468c2
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-01-31 11:28:24 +0330

    Adding essential fields & methods(Server class)

--- src/Miscs/Socket/Server.java
+++ src/Miscs/Socket/Server.java
@@ -1,4 +1,36 @@
 package Miscs.Socket;
 
+
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static Main.Main.*;
+
+@SuppressWarnings("FieldMayBeFinal")
 public class Server {
+
+    private static ExecutorService ClientPool = Executors.newFixedThreadPool(5);
+    private static int pointer = 0;
+    static ServerSocket listener;
+    int i = 0;
+
+
+    public Server() {
+
+    }
+
+
+    public void send(String SenderType, String ReceiverType, String[] Data) {
+
+    }
+
+
+    public void deleteClient(String Name) {
+
+    }
 }
