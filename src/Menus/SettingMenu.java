33f38943072af84835c7200c67b73ee33add3ffe
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-01 20:23:24 +0330

    Editing SettingMenu constructor fields based on recent changes

--- src/Menus/SettingMenu.java
+++ src/Menus/SettingMenu.java
@@ -21,10 +21,17 @@ public class SettingMenu extends JFrame {
     public SettingMenu(MainMenu main) {
         menu = main;
 
-        nameTXT = new JTextField(menu.username, 15);
+        mode = new JLabel();
         closeButton.setIcon(Icons.closeDialogIcon);
-        modeHButton.setIcon(Icons.modeHIcon);
-        modeNButton.setIcon(Icons.modeNOffIcon);
+        if (menu.player.difficulty == 0) {
+            modeHButton.setIcon(Icons.modeHIcon);
+            modeNButton.setIcon(Icons.modeNOffIcon);
+            mode.setText("Normal");
+        } else {
+            modeNButton.setIcon(Icons.modeNIcon);
+            modeHButton.setIcon(Icons.modeHOffIcon);
+            mode.setText("Hard");
+        }
         pane.setLayout(layout);
         JLabel pauseFrame = new JLabel();
         pauseFrame.setIcon(Icons.settingsIcon);
@@ -32,25 +39,28 @@ public class SettingMenu extends JFrame {
 
         muteCheckBox.addChangeListener(e -> {
             Game.mute = !muteCheckBox.isSelected();
-            if (!muteCheckBox.isSelected()) Sounds.mute();
+            if (!muteCheckBox.isSelected()) {
+                Sounds.mute();
+                Sounds.muted = true;
+            }
             else {
                 Sounds.muted = false;
-                Sounds.backPlay(Sounds.IN_GAME);
+                Sounds.backPlay(Sounds.MAIN_MENU);
             }
         });
         pane.add(muteCheckBox);
         pane.add(modeNButton);
         pane.add(closeButton);
         pane.add(modeHButton);
-        pane.add(nameTXT);
+        pane.add(mode);
         pane.add(nameLBL);
         pane.add(pauseFrame);
         layout.putConstraint(SpringLayout.WEST, muteCheckBox, 35, SpringLayout.WEST, pane);
         layout.putConstraint(SpringLayout.NORTH, muteCheckBox, 110, SpringLayout.NORTH, pane);
         layout.putConstraint(SpringLayout.WEST, nameLBL, 35, SpringLayout.WEST, pane);
         layout.putConstraint(SpringLayout.NORTH, nameLBL, 150, SpringLayout.NORTH, pane);
-        layout.putConstraint(SpringLayout.WEST, nameTXT, 105, SpringLayout.WEST, pane);
-        layout.putConstraint(SpringLayout.NORTH, nameTXT, 148, SpringLayout.NORTH, pane);
+        layout.putConstraint(SpringLayout.WEST, mode, 105, SpringLayout.WEST, pane);
+        layout.putConstraint(SpringLayout.NORTH, mode, 148, SpringLayout.NORTH, pane);
 
         layout.putConstraint(SpringLayout.WEST, closeButton, 481, SpringLayout.WEST, pane);
         layout.putConstraint(SpringLayout.NORTH, closeButton, 13, SpringLayout.NORTH, pane);
@@ -68,32 +78,27 @@ public class SettingMenu extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
-
-
     private void addClickListeners() {
         modeNButton.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
 
             }
-
             @Override
             public void mousePressed(MouseEvent e) {
 
             }
-
             @Override
             public void mouseReleased(MouseEvent e) {
-                menu.mode = 0;
+                menu.player.difficulty = 0;
                 modeNButton.setIcon(Icons.modeNOffIcon);
                 modeHButton.setIcon(Icons.modeHIcon);
+                mode.setText("Normal");
             }
-
             @Override
             public void mouseEntered(MouseEvent e) {
 
             }
-
             @Override
             public void mouseExited(MouseEvent e) {
 
@@ -104,24 +109,21 @@ public class SettingMenu extends JFrame {
             public void mouseClicked(MouseEvent e) {
 
             }
-
             @Override
             public void mousePressed(MouseEvent e) {
 
             }
-
             @Override
             public void mouseReleased(MouseEvent e) {
-                menu.mode = 1;
+                menu.player.difficulty = 1;
                 modeNButton.setIcon(Icons.modeNIcon);
                 modeHButton.setIcon(Icons.modeHOffIcon);
+                mode.setText("Hard");
             }
-
             @Override
             public void mouseEntered(MouseEvent e) {
 
             }
-
             @Override
             public void mouseExited(MouseEvent e) {
 
@@ -132,24 +134,18 @@ public class SettingMenu extends JFrame {
             public void mouseClicked(MouseEvent e) {
 
             }
-
             @Override
             public void mousePressed(MouseEvent e) {
 
             }
-
             @Override
             public void mouseReleased(MouseEvent e) {
-                if (nameTXT.getText().equals("")) nameTXT.setText(menu.username);
-                menu.username = nameTXT.getText();
                 dispose();
             }
-
             @Override
             public void mouseEntered(MouseEvent e) {
 
             }
-
             @Override
             public void mouseExited(MouseEvent e) {
 
@@ -157,3 +153,4 @@ public class SettingMenu extends JFrame {
         });
     }
 }
+
