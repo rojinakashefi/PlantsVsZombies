08489182594957be7ddd055849c4fbe7aa22e0aa
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-01 20:06:27 +0330

    Editing Cherry health (Cherry class)

--- src/Objects/Plants/Cherry.java
+++ src/Objects/Plants/Cherry.java
@@ -6,7 +6,7 @@ import java.awt.Container;
 public class Cherry extends Plant {
 
     public Cherry(Container c, int[] slut) {
-        super(c, 100, 3, slut[0], slut[1], 150);
+        super(c, 70, 3, slut[0], slut[1], 150);
         ready = new ImageIcon("gfx/cherry.pvz");
         setIcon(ready);
     }
