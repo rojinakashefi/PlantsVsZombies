9363b9f51c5fba4484f05dce6d1f6294e13a892f
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-01 20:09:55 +0330

    Editing SunFlower health(SunFlower class)

--- src/Objects/Plants/SunFlower.java
+++ src/Objects/Plants/SunFlower.java
@@ -7,7 +7,7 @@ import javax.swing.ImageIcon;
 public class SunFlower extends Plant {
 
     public SunFlower(Container c, int[] slut) {
-        super(c, 100, 20, slut[0], slut[1], 50);
+        super(c, 50, 20, slut[0], slut[1], 50);
         ready = new ImageIcon("gfx/sunflower.pvz");
         die = new ImageIcon("gfx/sunDie.pvz");
         setIcon(ready);
