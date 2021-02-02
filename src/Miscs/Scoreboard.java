35d036ce35177799706a64f82483205676799489
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-02 15:00:48 +0330

    Adding communicate method

--- src/Miscs/Scoreboard.java
+++ src/Miscs/Scoreboard.java
@@ -1,4 +1,29 @@
 package Miscs;
 
+import Miscs.Socket.Client;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static Main.Main.isServerUp;
+
+
 public class Scoreboard {
+
+    /**
+     * return the saved data on the server
+     */
+    public Scoreboard() {
+        Client score = new Client("Scoreboard");
+        communicate(score);
+    }
+
+    /**
+     * waits for a call, then returns the requested data
+     */
+
+    private void communicate(Client score) {
+
+    }
+
 }
