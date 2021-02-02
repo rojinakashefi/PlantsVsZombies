2178f375bec9ed913c63258170c3ae36501325f5
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-02 14:56:13 +0330

    Editing SavingMenu

--- src/Menus/SavingMenu.java
+++ src/Menus/SavingMenu.java
@@ -1,44 +1,82 @@
 package Menus;
 
 import Main.Main;
-import Miscs.GameSave;
+import Miscs.Sounds;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class SavingMenu extends JFrame{
+public class SavingMenu extends JFrame {
+    SpringLayout layout = new SpringLayout();
+    Container This = this.getContentPane();
+    JLabel headLabel;
+    JButton newSave = new JButton("New Save");
     public SavingMenu(Game game, PauseMenu pauseMenu) {
-        SpringLayout layout = new SpringLayout();
-        Container This = this.getContentPane();
+        headLabel = new JLabel("Saving Menu");
+        newSave.addActionListener(e -> {
+            Main.saves.add(game.save());
+            Sounds.mute();
+            new MainMenu(game.newLevel);
+            game.dispose();
+            pauseMenu.dispose();
+            dispose();
+        });
         this.setLayout(layout);
         this.setSize(340, 360);
-        JLabel headLabel = new JLabel("Saving Menu");
         layout.putConstraint(SpringLayout.NORTH, headLabel, 20, SpringLayout.NORTH, This);
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, headLabel, 0, SpringLayout.HORIZONTAL_CENTER, This);
+        layout.putConstraint(SpringLayout.WEST, headLabel, 20, SpringLayout.WEST, This);
+        layout.putConstraint(SpringLayout.NORTH, newSave, 20, SpringLayout.NORTH, This);
+        layout.putConstraint(SpringLayout.EAST, newSave, -20, SpringLayout.EAST, This);
         add(headLabel);
+        add(newSave);
+
+        if (Main.saves.size() > 0) showSaves(game, pauseMenu);
+        else {
+            JLabel emptyBox = new JLabel("No save files available!");
+            layout.putConstraint(SpringLayout.VERTICAL_CENTER, emptyBox, 0, SpringLayout.VERTICAL_CENTER, This);
+            layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, emptyBox, 0, SpringLayout.HORIZONTAL_CENTER, This);
+            add(emptyBox);
+        }
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public SavingMenu(MainMenu menu) {
+        headLabel = new JLabel("Loading Menu");
+        this.setLayout(layout);
+        this.setSize(340, 360);
+        layout.putConstraint(SpringLayout.NORTH, headLabel, 20, SpringLayout.NORTH, This);
+        layout.putConstraint(SpringLayout.WEST, headLabel, 20, SpringLayout.WEST, This);
+        layout.putConstraint(SpringLayout.NORTH, newSave, 20, SpringLayout.NORTH, This);
+        layout.putConstraint(SpringLayout.EAST, newSave, -20, SpringLayout.EAST, This);
+        add(headLabel);
+        add(newSave);
+        if (Main.saves.size() > 0) {
+            showSaves(menu);
+        }
+        else {
+            JLabel emptyBox = new JLabel("No save files available!");
+            layout.putConstraint(SpringLayout.VERTICAL_CENTER, emptyBox, 0, SpringLayout.VERTICAL_CENTER, This);
+            layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, emptyBox, 0, SpringLayout.HORIZONTAL_CENTER, This);
+            add(emptyBox);
+        }
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    private void showSaves(Game game, PauseMenu pauseMenu) {
         JPanel panel = new JPanel(layout);
-        panel.setSize(300, 300);
-        JScrollPane scrollPane = new JScrollPane(panel);
-        add(scrollPane);
+        layout.putConstraint(SpringLayout.NORTH, panel, 20, SpringLayout.NORTH, headLabel);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, panel, 0, SpringLayout.HORIZONTAL_CENTER, This);
+        panel.setBackground(Color.gray);
+        panel.setPreferredSize(new Dimension(300, 300));
+        getContentPane().add(panel, SpringLayout.SOUTH);
+        JButton j = new JButton("ss");
+        panel.add(j);
         for (int i = 0; i < Main.saves.size(); i++) {
-            JPanel temp = new JPanel(layout);
-            temp.setSize(295, 95);
-            panel.add(temp);
-            panel.setName(String.valueOf(i));
-            layout.putConstraint(SpringLayout.NORTH, temp, 5 + (95 * i), SpringLayout.NORTH, panel);
-            layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, temp, 0, SpringLayout.HORIZONTAL_CENTER, panel);
-
-            JLabel name = new JLabel("Game Save " + (i + 1));
-            layout.putConstraint(SpringLayout.WEST, name, 10, SpringLayout.WEST, temp);
-            layout.putConstraint(SpringLayout.VERTICAL_CENTER, name, 0, SpringLayout.VERTICAL_CENTER, temp);
-            temp.add(name);
-
-            JLabel time = new JLabel("Elapsed Time " + Main.saves.get(i).gameTime);
-            layout.putConstraint(SpringLayout.WEST, time, 50, SpringLayout.WEST, name);
-            layout.putConstraint(SpringLayout.VERTICAL_CENTER, time, 0, SpringLayout.VERTICAL_CENTER, temp);
-            temp.add(time);
+            JPanel temp = addPanel(panel, i);
             int finalI = i;
             temp.addMouseListener(new MouseListener() {
                 @Override
@@ -51,8 +89,51 @@ public class SavingMenu extends JFrame{
                 }
                 @Override
                 public void mouseReleased(MouseEvent e) {
-                    Main.saves.remove(finalI);
-                    Main.saves.add(game.save());
+                    Sounds.mute();
+                    Main.saves.set(finalI, game.save());
+                    new MainMenu(game.newLevel);
+                    game.dispose();
+                    pauseMenu.dispose();
+                    dispose();
+                }
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+            panel.repaint();
+            repaint();
+        }
+    }
+
+    private void showSaves(MainMenu menu) {
+        JPanel panel = new JPanel(layout);
+        layout.putConstraint(SpringLayout.NORTH, panel, 20, SpringLayout.NORTH, headLabel);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, panel, 0, SpringLayout.HORIZONTAL_CENTER, This);
+        panel.setPreferredSize(new Dimension(300, 300));
+        this.add(new JScrollPane(panel));
+        for (int i = 0; i < Main.saves.size(); i++) {
+            JPanel temp = addPanel(panel, i);
+            int finalI = i;
+            temp.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    Sounds.mute();
+                    new Game(Main.saves.get(finalI), menu.player, Sounds.muted);
+                    menu.dispose();
+                    dispose();
                 }
                 @Override
                 public void mouseEntered(MouseEvent e) {
@@ -64,8 +145,27 @@ public class SavingMenu extends JFrame{
                 }
             });
         }
-        layout.putConstraint(SpringLayout.NORTH, panel, 20, SpringLayout.NORTH, headLabel);
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, panel, 0, SpringLayout.HORIZONTAL_CENTER, This);
-        panel.setBackground(Color.darkGray);
+    }
+
+    private JPanel addPanel(JPanel panel, int i) {
+        JPanel temp = new JPanel(layout);
+        temp.setPreferredSize(new Dimension(295, 95));
+
+        panel.add(temp);
+        panel.setName(String.valueOf(i));
+        temp.setBackground(Color.gray);
+        layout.putConstraint(SpringLayout.NORTH, temp, 5 + (95 * i), SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, temp, 0, SpringLayout.HORIZONTAL_CENTER, panel);
+
+        JLabel name = new JLabel("Game Save " + (i + 1));
+        layout.putConstraint(SpringLayout.WEST, name, 10, SpringLayout.WEST, temp);
+        layout.putConstraint(SpringLayout.VERTICAL_CENTER, name, 0, SpringLayout.VERTICAL_CENTER, temp);
+        temp.add(name);
+
+        JLabel time = new JLabel("Elapsed Time " + Main.saves.get(i).gameTime);
+        layout.putConstraint(SpringLayout.WEST, time, 50, SpringLayout.WEST, name);
+        layout.putConstraint(SpringLayout.VERTICAL_CENTER, time, 0, SpringLayout.VERTICAL_CENTER, temp);
+        temp.add(time);
+        return temp;
     }
 }
