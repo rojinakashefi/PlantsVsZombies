accb235f6da61d99b45ac8d6fbc4b078ac8c1b91
rojinakashefi <rojina.kashefi@yahoo.com>
2021-02-03 17:12:25 +0330

    Adding normal Zombie JavaDoc

--- src/Objects/Zombies/Normal.java
+++ src/Objects/Zombies/Normal.java
@@ -1,14 +1,15 @@
 package Objects.Zombies;
-
 import Menus.Game;
 import Miscs.Icons;
 import Miscs.Sounds;
-
 import javax.swing.Timer;
 import javax.swing.ImageIcon;
 import java.awt.Container;
 
-
+/**
+ * It normal zombie Class
+ * it health is 200 and it speed is 2 (0.5 per second)it damages is 5
+ */
 public class Normal extends Zombie {
 
     public Normal(Container c, int row) {
@@ -18,6 +19,11 @@ public class Normal extends Zombie {
         setIcon(walk);
     }
 
+    /**
+     * LossHealth method
+     * @param Amount as amount it will decrease from health of zombies
+     * @param isFrozen is frozen if pea is frozen
+     */
     @Override
     public void lossHealth(int Amount, boolean isFrozen) {
         health -= Amount;
