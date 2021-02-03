537a366c6a8c57f33e8e7724c7ef5ee01859030b
rojinakashefi <rojina.kashefi@yahoo.com>
2021-02-03 17:14:00 +0330

    Adding Football JavaDoc

--- src/Objects/Zombies/Football.java
+++ src/Objects/Zombies/Football.java
@@ -1,11 +1,14 @@
 package Objects.Zombies;
-
 import Miscs.Icons;
 import Miscs.Sounds;
-
 import javax.swing.ImageIcon;
 import java.awt.Container;
 
+/**
+ * Football class
+ * its health is 1000 and speed 6 and damage 15
+ * @author RojinaKashefi && HliahashemiPOur
+ */
 public class Football extends Zombie {
 
     public Football(Container c, int row) {
@@ -18,19 +21,19 @@ public class Football extends Zombie {
         hitSound = Sounds.PLASTIC;
     }
 
+    /**
+     * LossHealth Method
+     *
+     * @param Amount   as amount it will decrease from health of zombies
+     * @param isFrozen is frozen if pea is frozen
+     */
     @Override
     public void lossHealth(int Amount, boolean isFrozen) {
         health -= Amount;
         if (health > 0) {
             Sounds.play(hitSound);
-        }
-        else {
+        } else {
             kill(false);
-        }
-        if (isFrozen && !frozen) {
-            speed /= 2;
-            frozen = true;
-            Sounds.play(Sounds.FREEZE);
         }
     }
 }
