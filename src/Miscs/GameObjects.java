526ce9338a18136c6eb3eacc8af7281e26f0f871
Helia Hashemipour <heliahashemipour2@gmail.com>
2021-02-03 20:18:36 +0330

    Adding Coordination class javadoc

--- src/Miscs/GameObjects.java
+++ src/Miscs/GameObjects.java
@@ -2,11 +2,22 @@ package Miscs;
 
 import java.awt.*;
 
+/**
+ * This class sets the situation of each objects.
+ * @author Hashemipour
+ * @since 2021
+ */
 public class GameObjects {
     public String objectName;
     public int health;
     public Point position;
 
+    /**
+     * Main constructor of the GameObjects.class
+     * @param objectName object name.
+     * @param position   the position of each object.
+     * @param health     the number of health.
+     */
     public GameObjects(String objectName, Point position, int health) {
         this.objectName = objectName;
         this.position = position;
